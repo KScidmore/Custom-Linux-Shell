6a5ccac14238c6da74ce58ba3205689cfeb0965d
kscid125 <kscid125@mtroyal.ca>
2024-09-26 18:26:11 +0000

    Broken plz fix

--- echo.c
+++ echo.c
@@ -1,16 +1,25 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include  <stdio.h>
 
-int stringComp(const char * str1, const char * str2);
+#define BUFFER_SIZE 1024
+#define MAX_TOKENS 256
+
+int stringComp(const char *str1, const char *str2);
+void tokenize(char *tokens[], char buffer[]);
 
 int main(){
 
-    const char * dollar = "$ ";
-    char buffer[1024];
+    const char *dollar = "$ ";
+    char buffer[BUFFER_SIZE];
+    char *tokens[MAX_TOKENS];
     ssize_t bytesRead;
 
-    write(1, dollar, 2);
+    int i, j;
 
+    printf("made it");
+
+    write(1, dollar, 2);
     while(1){
 
         bytesRead = read(0, buffer, 1024 - 1);
@@ -20,11 +29,59 @@ int main(){
             break;
         }
         write(1, buffer, bytesRead);
+
+        tokenize(tokens, buffer);
+
+        for(i = 0; tokens[i] != 0; i++){
+            j = 0;
+            while(&tokens[i][j] != "\0"){
+                write(1, &tokens[i][j], 1);
+            }
+
+            write(1, "\n" , 1);
+        }
         write(1, dollar, 2);
     }
     return 0;
 }
 
+void tokenize(char *tokens[], char buffer[]){
+
+    int i = 0;
+    int tokenIndex = 0;
+    char *start = 0;
+
+    while(&buffer[i] != "\0"){
+
+        if(&buffer[i] != " " && &buffer[i] != "\n"){
+
+            if(start == 0){
+
+                start = &buffer[i];
+            }
+        }
+        else{
+
+            if(start != 0){
+
+                buffer[i] = '\0';
+                tokens[tokenIndex++] = start;
+                start = 0;
+            }
+        }
+
+        if(start != 0){
+
+            tokens[tokenIndex++] = start;
+
+        }
+
+        tokens[tokenIndex] = 0;
+
+    }
+
+
+}
 int stringComp(const char * str1,const char * str2){
 
     while(*str1 && *str2){
@@ -44,3 +101,4 @@ int stringComp(const char * str1,const char * str2){
     return 1;
     
 }
+
